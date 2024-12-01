4458f7063bb2917515b1b6e2b2c5401a19d46839
Adrian Drzazgowski <97746159+iamdrzazgowski@users.noreply.github.com>
2024-12-01 19:06:24 +0100

    Finished program

--- main.cpp
+++ main.cpp
@@ -42,6 +42,18 @@ bool openSerialPort(const char * port){
     return true;
 }
 
+void printSerialPortConfig(){
+
+    GetCommState(handle, &dcb);
+
+    cout << "\nKonfiguracja portu: " << endl;
+    cout << "BaudRate: " << dcb.BaudRate << endl;
+    cout << "Parity: " << int(dcb.Parity) << endl;
+    cout << "StopBits: " << int(dcb.StopBits) << endl;
+    cout << "ByteSize: " << int(dcb.ByteSize) << endl;
+    cout << endl;
+}
+
 bool serialPortConfig(){
 
     if(GetCommState(handle, &dcb) == 0){
@@ -51,14 +63,178 @@ bool serialPortConfig(){
     }
 
     dcb.DCBlength = sizeof(dcb);
-    dcb.BaudRate = CBR_256000;
+//    dcb.BaudRate = CBR_256000;
     dcb.fParity = FALSE;
-    dcb.Parity = 2;
-    dcb.StopBits = ONESTOPBIT;
-    dcb.ByteSize = 8;
+//    dcb.Parity = 2;
+//    dcb.StopBits = ONESTOPBIT;
+//    dcb.ByteSize = 8;
     dcb.fDtrControl = DTR_CONTROL_ENABLE;
 
+    int  baudRateOption = 0;
+    cout << "Opcje dla parametru BaudRate: " << endl;
+    cout << "1. CBR_110\n"
+            "2. CBR_300\n"
+            "3. CBR_600\n"
+            "4. CBR_1200\n"
+            "5. CBR_2400\n"
+            "6. CBR_4800\n"
+            "7. CBR_9600\n"
+            "8. CBR_14400\n"
+            "9. CBR_19200\n"
+            "10. CBR_38400\n"
+            "11. CBR_56000\n"
+            "12. CBR_57600\n"
+            "13. CBR_115200\n"
+            "14. CBR_128000\n"
+            "15. CBR_256000\n"<< endl;
+
+
+    cout << "Wybierz wartosc parametru BaudRate: ";
+    cin >> baudRateOption;
+    cout << endl;
+    
+    switch (baudRateOption) {
+        case 1:
+            dcb.BaudRate = CBR_110;
+            break;
+        case 2:
+            dcb.BaudRate = CBR_300;
+            break;
+        case 3:
+            dcb.BaudRate = CBR_600;
+            break;
+        case 4:
+            dcb.BaudRate = CBR_1200;
+            break;
+        case 5:
+            dcb.BaudRate = CBR_2400;
+            break;
+        case 6:
+            dcb.BaudRate = CBR_4800;
+            break;
+        case 7:
+            dcb.BaudRate = CBR_9600;
+            break;
+        case 8:
+            dcb.BaudRate = CBR_14400;
+            break;
+        case 9:
+            dcb.BaudRate = CBR_19200;
+            break;
+        case 10:
+            dcb.BaudRate = CBR_38400;
+            break;
+        case 11:
+            dcb.BaudRate = CBR_56000;
+            break;
+        case 12:
+            dcb.BaudRate = CBR_57600;
+            break;
+        case 13:
+            dcb.BaudRate = CBR_115200;
+            break;
+        case 14:
+            dcb.BaudRate = CBR_128000;
+            break;
+        case 15:
+            dcb.BaudRate = CBR_256000;
+            break;
+        default: cout << "Niepoprawna wartosc! Ustawiono domysla wartosc BoudRate na CBR_115200" << endl;
+            dcb.BaudRate=CBR_115200;
+            break;
+    }
+
+    int parityOption = 0;
+    cout << "Opcje dla parametru Parity:\n"
+            "1. NOPARITY\n"
+            "2. ODDPARITY\n"
+            "3. EVENPARITY\n"
+            "4. MARKPARITY\n"
+            "5. SPACEPARITY\n"<< endl;
+
+
+    cout << "Wybierz wartosc parametru Parity: ";
+    cin >> parityOption;
+    cout << endl;
+
+    switch(parityOption) {
+        case 1:
+            dcb.Parity = NOPARITY;
+            break;
+        case 2:
+            dcb.Parity = ODDPARITY;
+            break;
+        case 3:
+            dcb.Parity = EVENPARITY;
+            break;
+        case 4:
+            dcb.Parity = MARKPARITY;
+            break;
+        case 5:
+            dcb.Parity = SPACEPARITY;
+            break;
+        default: cout << "Niepoprawna wartosc! Ustawiono domysla wartosc Parity na EVENPARITY" << endl;
+            dcb.Parity = 2;
+            break;
+    }
+
+    int stopBitsOption = 0;
+    cout << "Opcje dla parametru StopBits:\n"
+            "1. ONESTOPBIT\n"
+            "2. ONE5STOPBITS\n"
+            "3. TWOSTOPBITS\n"<< endl;
+
+
+    cout << "Wybierz wartosc parametru StopBits: ";
+    cin >> stopBitsOption;
+    cout << endl;
+
+    switch(stopBitsOption) {
+        case 1:
+            dcb.StopBits = ONESTOPBIT;
+            break;
+        case 2:
+            dcb.StopBits = ONE5STOPBITS;
+            break;
+        case 3:
+            dcb.StopBits = TWOSTOPBITS;
+            break;
+        default: cout << "Niepoprawna wartosc! Ustawiono domysla wartosc StopBits na ONESTOPBIT" << endl;
+            dcb.StopBits = ONESTOPBIT;
+            break;
+    }
+
+    int byteSizeOption = 0;
+    cout << "Opcje dla parametru ByteSize:\n"
+            "1. 5\n"
+            "2. 6\n"
+            "3. 7\n"
+            "4. 8\n"<< endl;
+
+    cout << "Wybierz wartosc parametru ByteSize: ";
+    cin >> byteSizeOption;
+    cout << endl;
+
+    switch(byteSizeOption) {
+        case 1:
+            dcb.ByteSize = 5;
+            break;
+        case 2:
+            dcb.ByteSize = 6;
+            break;
+        case 3:
+            dcb.ByteSize = 7;
+            break;
+        case 4:
+            dcb.ByteSize = 8;
+            break;
+        default: cout << "Niepoprawna wartosc! Ustawiono domysla wartosc ByteSize na 8" << endl;
+            dcb.ByteSize = 8;
+            break;
+    }
+
     SetCommState(handle, &dcb);
+    printSerialPortConfig();
 
     return true;
 }
@@ -84,8 +260,11 @@ bool setTimeoutsConfig(unsigned long ReadIntervalTimeout, unsigned long ReadTota
 
 
 bool sendString(){
-    char bufferIn[1024] = {0};
-    char bufferOut[1024] = {0};
+
+    const long buffSize = 2048;
+
+    char bufferIn[buffSize] = {0};
+    char bufferOut[buffSize] = {0};
 
     cout << "Podaj tekst do wyslania: ";
     cin.ignore();
@@ -117,6 +296,7 @@ bool sendFile(){
     FILE * destFile;
 
     const long buffSize = 2048;
+//    C:\Users\hello\Desktop
 
     char bufferIn[buffSize] = {0};
     char bufferOut[buffSize] = {0};
@@ -186,11 +366,13 @@ int main() {
     if(openSerialPort(port)){
         GetCommState(handle, &dcb);
 
+//        Konfiguracja portu
         serialPortConfig();
 
         SetCommState(handle, &dcb);
 
         GetCommTimeouts(handle, &commTimeouts);
+//        Ustawienie timeoutów
         setTimeoutsConfig(0,0,1000,0,0);
         SetCommTimeouts(handle, &commTimeouts);
 
@@ -213,6 +395,7 @@ int main() {
     };
 
     std::cout << "Nacisnij Enter, aby zamknac program..." << std::endl;
+    std::cin.get();
     std::cin.get();
 
     return 0;
